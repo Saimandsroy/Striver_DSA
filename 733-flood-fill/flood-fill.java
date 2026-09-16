952f69a2ba939fdf7b8ca51e4d9c53ca5c1d75a7
Saimands Roy <158268906+Saimandsroy@users.noreply.github.com>
2026-09-16 10:01:10 +0530

    Time: 2 ms (10.84%) | Memory: 46.6 MB (89.67%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,68 +1,59 @@
 class Solution {
-    class Pair{
+
+    class Pair {
         int row;
         int col;
-        int target;
 
-        Pair(int row, int col, int target){
-            this.row=row;
-            this.col=col;
-            this.target=target;
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-
     }
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
-        int m=image.length; 
-        int n=image[0].length;
-        int [][] vis= new int[m][n];
+        int m = image.length;
+        int n = image[0].length;
+
+        int originalColor = image[sr][sc];
+
+
+        if (originalColor == color) {
+            return image;
+        }
 
         Queue<Pair> queue = new LinkedList<>();
 
-        for(int i=0; i<m ; i++){
-            for(int j=0; j<n ;j++){
-                if(i==sr && j==sc){
-                    int target=image[i][j];
-                    queue.add(new Pair(i,j,target));
-                    vis[i][j]=color;
-                }else{
-                    vis[i][j]=image[i][j];
+
+        image[sr][sc] = color;
+        queue.add(new Pair(sr, sc));
+
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+
+        while (!queue.isEmpty()) {
+
+            int r = queue.peek().row;
+            int c = queue.peek().col;
+
+            queue.remove();
+
+            for (int i = 0; i < 4; i++) {
+
+                int nRow = r + dRow[i];
+                int nCol = c + dCol[i];
+
+                if (nRow >= 0 && nRow < m &&
+                    nCol >= 0 && nCol < n &&
+                    image[nRow][nCol] == originalColor) {
+
+                    image[nRow][nCol] = color;
+
+                    queue.add(new Pair(nRow, nCol));
                 }
             }
         }
 
-
-        int [] dRow={-1,0,+1,0};
-        int [] dCol={0,+1,0,-1};
-
-        
-        while(!queue.isEmpty()){
-            int r=queue.peek().row;
-            int c=queue.peek().col;
-            int t=queue.peek().target;
-
-
-            queue.remove();
-            
-        for(int i=0; i<4; i++){
-            int nRow=r+dRow[i];
-            int nCol=c+dCol[i];
-
-            if(nRow>=0 && nRow<m &&
-                nCol>=0 && nCol<n &&
-                image[nRow][nCol]==t &&
-                vis[nRow][nCol] !=color
-            ){
-                queue.add(new Pair(nRow, nCol, t));
-                vis[nRow][nCol]=color;
-            }
-
-        }
-            
-        }
-
-    return vis;
-      
-        
+        return image;
     }
 }
