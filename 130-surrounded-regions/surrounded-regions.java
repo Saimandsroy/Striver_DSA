f9a2193f79cb4305c21c5cc633872fed16f49cc0
Saimands Roy <158268906+Saimandsroy@users.noreply.github.com>
2026-09-17 11:05:59 +0530

    Time: 3 ms (29.59%) | Memory: 48.1 MB (10.98%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,62 +1,76 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
 
-    public void dfs(int row, int col, char[][] board) {
-
-        if (row < 0 || row >= board.length ||
-                col < 0 || col >= board[0].length ||
-                board[row][col] != 'O') {
-            return;
+        Pair(int row, int col){
+            this.row=row;
+            this.col=col;
         }
-
-        board[row][col] = '#';
-
-        dfs(row - 1, col, board);
-        dfs(row + 1, col, board);
-        dfs(row, col - 1, board);
-        dfs(row, col + 1, board);
     }
-
     public void solve(char[][] board) {
 
-        int m = board.length;
-        int n = board[0].length;
+        int m=board.length;
+        int n=board[0].length;
+        
+        Queue<Pair> queue= new LinkedList<>();
 
-        for (int i = 0; i < n; i++) {
-            if (board[0][i] == 'O') {
-                dfs(0, i, board);
+        for(int i=0; i<n; i++){
+            if(board[0][i]=='O'){
+                queue.add(new Pair(0,i));
+                 board[0][i] = '#';
             }
 
-            if (board[m - 1][i] == 'O') {
-                dfs(m - 1, i, board);
+            if(board[m-1][i]=='O'){
+                queue.add(new Pair(m-1,i));
+                 board[m-1][i] = '#';
             }
-
         }
 
-        for (int i = 0; i < m; i++) {
-
-            if (board[i][0] == 'O') {
-                dfs(i, 0, board);
+        for(int i=0; i<m; i++){
+            if(board[i][0]=='O'){
+                queue.add(new Pair(i,0));
+                 board[i][0] = '#';
             }
 
-            if (board[i][n - 1] == 'O') {
-                dfs(i, n - 1, board);
+            if(board[i][n-1]=='O'){
+                queue.add(new Pair(i,n-1));
+                 board[i][n-1] = '#';
             }
-
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int [] dRow={-1,0,+1,0};
+        int [] dCol={0,1,0,-1};
+        while(!queue.isEmpty()){
 
-                if (board[i][j] == 'O') {
-                    board[i][j] = 'X';
+            Pair current= queue.poll();
+
+            int row=current.row;
+            int col=current.col;
+
+            for(int i=0; i<4; i++){
+                int nRow=row+dRow[i];
+                int nCol=col+dCol[i];
+
+                if(nRow>=0 && nRow<m && nCol>=0 && nCol<n &&
+                    board[nRow][nCol]=='O'
+                ){
+                    queue.add(new Pair(nRow, nCol));
+                    board[nRow][nCol]='#';
                 }
+            }
 
-                else if (board[i][j] == '#') {
-                    board[i][j] = 'O';
+        }
+
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
+                }else if(board[i][j]=='#'){
+                    board[i][j]='O';
                 }
             }
         }
-
     }
-
 }
