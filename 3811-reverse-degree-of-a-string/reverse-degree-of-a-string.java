3bf2c8667b70c381234a7c06c224879880a74bac
Saimands Roy <158268906+Saimandsroy@users.noreply.github.com>
2026-09-21 00:04:48 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (81.16%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverseDegree(String s) {
+        
+
+        int sum=0;
+
+        for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+            
+            int reverse= 26-(s.charAt(i)-'a');
+
+            sum+=(reverse*(i+1));
+        }
+        return sum;
+    }
+}
